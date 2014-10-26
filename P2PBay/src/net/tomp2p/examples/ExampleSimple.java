bc08104a6fbff70ae327338a2fcdae3530419323
Henrique Rocha <hdlopesrocha91@gmail.com>
2014-10-26 19:42:56 +0000

    [ADD] disabled logger and added shell script to run ExampleSimple

--- P2PBay/src/net/tomp2p/examples/ExampleSimple.java
+++ P2PBay/src/net/tomp2p/examples/ExampleSimple.java
@@ -1,14 +1,23 @@
 package net.tomp2p.examples;
 
 import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.tomp2p.futures.FutureDHT;
 import net.tomp2p.futures.FutureBootstrap; 
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.storage.Data;
+
+
 public class ExampleSimple {
     final private Peer peer;
+    final static Logger logger = LoggerFactory.getLogger(ExampleSimple.class);
+    
+    
     public ExampleSimple(int peerId) throws Exception {
         peer = new PeerMaker(Number160.createHash(peerId)).setPorts(4000 + peerId).makeAndListen();
         FutureBootstrap fb = peer.bootstrap().setBroadcast().setPorts(4001).start();
@@ -17,15 +26,24 @@ public class ExampleSimple {
             peer.discover().setPeerAddress(fb.getBootstrapTo().iterator().next()).start().awaitUninterruptibly();
         }
     }
+    
     public static void main(String[] args) throws NumberFormatException, Exception {
+    	
+    
         ExampleSimple dns = new ExampleSimple(Integer.parseInt(args[0]));
         if (args.length == 3) {
-            dns.store(args[1], args[2]);
+        	System.out.println("********************* PUT ***********************");
+        	dns.store(args[1], args[2]);
+        	System.out.println("********************* OK! ***********************");
+
         }
         if (args.length == 2) {
+        	System.out.println("********************* GET ***********************");
             System.out.println("Name:" + args[1] + " IP:" + dns.get(args[1]));
+        	System.out.println("********************* OK! ***********************");
         }
     }
+    
     private String get(String name) throws ClassNotFoundException, IOException {
         FutureDHT futureDHT = peer.get(Number160.createHash(name)).start();
         futureDHT.awaitUninterruptibly();
@@ -34,6 +52,7 @@ public class ExampleSimple {
         }
         return "not found";
     }
+    
     private void store(String name, String ip) throws IOException {
         peer.put(Number160.createHash(name)).setData(new Data(ip)).start().awaitUninterruptibly();
     }
