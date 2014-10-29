26bbcacc11891378eb9abb3b6af10a5d36846155
Henrique Rocha <hdlopesrocha91@gmail.com>
2014-10-29 18:50:40 +0000

    [CLS] javardoc

--- P2PBay/src/ist/p2p/P2PBay.java
+++ P2PBay/src/ist/p2p/P2PBay.java
@@ -19,9 +19,14 @@ import net.tomp2p.p2p.PeerMaker;
 import net.tomp2p.peers.Number160;
 import net.tomp2p.storage.Data;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class P2PBay.
+ */
 public class P2PBay {
 
-    static private Peer peer=null;
+	/** The peer. */
+	static private Peer peer = null;
 
 	/** The Constant random. */
 	private static final Random RANDOM = new Random();
@@ -45,139 +50,204 @@ public class P2PBay {
 		return str;
 	}
 
-	   static String sha1(String input) {
-		   StringBuffer sb = new StringBuffer();
-		   
-			try {
-				MessageDigest mDigest = MessageDigest.getInstance("SHA1");
-				byte[] result = mDigest.digest(input.getBytes());
-				for (int i = 0; i < result.length; i++) {
-					sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-				}
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	/**
+	 * Sha1.
+	 *
+	 * @param input
+	 *            the input
+	 * @return the string
+	 */
+	static String sha1(String input) {
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+			byte[] result = mDigest.digest(input.getBytes());
+			for (int i = 0; i < result.length; i++) {
+				sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16)
+						.substring(1));
 			}
-	         
-	        return sb.toString();
-	    }
-	
-	public static boolean argExists(String arg, String [] args){
-		for(int i=0; i < args.length ; ++i){
-			if(args[i].equals(arg)){
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * Arg exists.
+	 *
+	 * @param arg
+	 *            the arg
+	 * @param args
+	 *            the args
+	 * @return true, if successful
+	 */
+	public static boolean argExists(String arg, String[] args) {
+		for (int i = 0; i < args.length; ++i) {
+			if (args[i].equals(arg)) {
 				return true;
 			}
 		}
-		return false;		
+		return false;
 	}
-    
-    
-	public static String getArgValue(String arg, String [] args){
-		for(int i=0; i < args.length ; ++i){
-			if(args[i].equals(arg) && i+1 < args.length){
-				return args[i+1];
+
+	/**
+	 * Gets the arg value.
+	 *
+	 * @param arg
+	 *            the arg
+	 * @param args
+	 *            the args
+	 * @return the arg value
+	 */
+	public static String getArgValue(String arg, String[] args) {
+		for (int i = 0; i < args.length; ++i) {
+			if (args[i].equals(arg) && i + 1 < args.length) {
+				return args[i + 1];
 			}
 		}
-		return null;		
+		return null;
 	}
-	
-	
-	public static void loadUsersFile(String filename) throws IOException{
+
+	/**
+	 * Load users file.
+	 *
+	 * @param filename
+	 *            the filename
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void loadUsersFile(String filename) throws IOException {
 		File file = new File(filename);
 		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String line="";
-		
-		while((line=reader.readLine())!=null){
-			String [] splits = line.split(":");
+		String line = "";
+
+		while ((line = reader.readLine()) != null) {
+			String[] splits = line.split(":");
 			String salt = randomString(16);
-			String hash = sha1(salt+splits[1]);
-			
-			store("user:"+splits[0],salt+":"+ hash);
+			String hash = sha1(salt + splits[1]);
+
+			store("user:" + splits[0], salt + ":" + hash);
 		}
-		
-		
+
 		reader.close();
-		
+
 	}
-	
-    private static Object get(String key) throws ClassNotFoundException, IOException {
-        FutureDHT futureDHT = peer.get(Number160.createHash(key)).start();
-        futureDHT.awaitUninterruptibly();
-        if (futureDHT.isSuccess()) {
-            return futureDHT.getData().getObject();
-        }
-        return null;
-    }
-    
-    private static void store(String key, Object value) throws IOException {
-        peer.put(Number160.createHash(key)).setData(new Data(value)).start().awaitUninterruptibly();
-    }
-	
-	
+
+	/**
+	 * Gets the.
+	 *
+	 * @param key
+	 *            the key
+	 * @return the object
+	 * @throws ClassNotFoundException
+	 *             the class not found exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	private static Object get(String key) throws ClassNotFoundException,
+			IOException {
+		FutureDHT futureDHT = peer.get(Number160.createHash(key)).start();
+		futureDHT.awaitUninterruptibly();
+		if (futureDHT.isSuccess()) {
+			return futureDHT.getData().getObject();
+		}
+		return null;
+	}
+
+	/**
+	 * Store.
+	 *
+	 * @param key
+	 *            the key
+	 * @param value
+	 *            the value
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	private static void store(String key, Object value) throws IOException {
+		peer.put(Number160.createHash(key)).setData(new Data(value)).start()
+				.awaitUninterruptibly();
+	}
+
 	// -i ip:port of known peer (bootstrap if not exists)
 	// -u loads users file
-	
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
 	public static void main(String[] args) throws IOException {
 		String usersFileName = getArgValue("-u", args);
 
-		Random  random = new Random();
-		int myPeerPort = !argExists("-i",args) ? 1024: 1025 + random.nextInt(60000);
-		
+		Random random = new Random();
+		int myPeerPort = !argExists("-i", args) ? 1024 : 1025 + random
+				.nextInt(60000);
+
 		Bindings bindings = new Bindings();
 		bindings.addInterface("eth0");
 		bindings.addInterface("wlan0");
 		bindings.addInterface("lo");
 
-        peer = new PeerMaker(new Number160(random)).setPorts(myPeerPort).setBindings(bindings).setEnableIndirectReplication(true).makeAndListen();
-        peer.getConfiguration().setBehindFirewall(true);
-        
-        if(argExists("-i",args)){
-    		String [] knownPeerIpPort = getArgValue("-i", args).split(":");
-    		int knownPeerPort = Integer.valueOf(knownPeerIpPort[1]);
-    		String knownPeerIp = knownPeerIpPort[0];
-        	InetAddress address = Inet4Address.getByName(knownPeerIp);
-        	FutureDiscover futureDiscover = peer.discover().setInetAddress(address).setPorts(knownPeerPort).start();
-        	futureDiscover.awaitUninterruptibly();
-        }
-      
-		
-    	if(usersFileName!=null){
-    		loadUsersFile(usersFileName);        		
-    	}
-		
+		peer = new PeerMaker(new Number160(random)).setPorts(myPeerPort)
+				.setBindings(bindings).setEnableIndirectReplication(true)
+				.makeAndListen();
+		peer.getConfiguration().setBehindFirewall(true);
+
+		if (argExists("-i", args)) {
+			String[] knownPeerIpPort = getArgValue("-i", args).split(":");
+			int knownPeerPort = Integer.valueOf(knownPeerIpPort[1]);
+			String knownPeerIp = knownPeerIpPort[0];
+			InetAddress address = Inet4Address.getByName(knownPeerIp);
+			FutureDiscover futureDiscover = peer.discover()
+					.setInetAddress(address).setPorts(knownPeerPort).start();
+			futureDiscover.awaitUninterruptibly();
+		}
+
+		if (usersFileName != null) {
+			loadUsersFile(usersFileName);
+		}
+
 		System.out.println("***********************");
-		
-		
+
 		commandLine();
-		
+
 	}
 
-	public static void commandLine(){
+	/**
+	 * Command line.
+	 */
+	public static void commandLine() {
 		Scanner scanner = new Scanner(System.in);
 		Boolean authenticated = false;
-		
-		while(!authenticated){
-		
+
+		while (!authenticated) {
+
 			System.out.print("Username: ");
 			String username = scanner.nextLine();
-			
+
 			System.out.print("Password: ");
 			String password = scanner.nextLine();
-			
+
 			try {
-				String saltPlusHash =(String) get("user:"+username);	
-				if(saltPlusHash !=null){
-					String [] splits = saltPlusHash.split(":");
-					if(splits.length==2){
+				String saltPlusHash = (String) get("user:" + username);
+				if (saltPlusHash != null) {
+					String[] splits = saltPlusHash.split(":");
+					if (splits.length == 2) {
 						String salt = splits[0];
 						String hash = splits[1];
 
-						if(sha1(salt+password).equals(hash)){
-							authenticated=true;
+						if (sha1(salt + password).equals(hash)) {
+							authenticated = true;
 						}
 					}
-				}
-				else {
+				} else {
 					System.err.println("Authentication failed!");
 				}
 			} catch (ClassNotFoundException | IOException e) {
@@ -188,7 +258,5 @@ public class P2PBay {
 		System.out.println("cenas!");
 		scanner.close();
 	}
-	
-	
-	
+
 }
