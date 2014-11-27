7ec6569e888cfb1e56322b18668f57bc0a98ad7d
AngeloPingo <angelopingosk8@gmail.com>
2014-11-27 00:06:59 +0000

    diz ai cenqs

--- Lab7/src/seprs/unstructured/SearchMessage.java
+++ Lab7/src/seprs/unstructured/SearchMessage.java
@@ -1,18 +1,39 @@
 package seprs.unstructured;
 
+import java.awt.List;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class SearchMessage implements Serializable, Message {
 	
 	String word;
+	int ID;
+	ArrayList<String> peers = new ArrayList<String>();
 
 	public SearchMessage( String src) {
 		word = src;
 	}
 	
+	public SearchMessage(String src, int IDtemp) {
+		word = src;
+		this.ID = IDtemp;
+	}
+	
 	public String getWord() {
-		return word;
+		return this.word;
+	}
+	
+	public ArrayList<String> getpeers() {
+		return this.peers;
+	}
+	
+	public int getID() {
+		return this.ID;
+	}
+	
+	public void addPeer(String peerSend) {
+		peers.add(peerSend);
 	}
 	
 	@Override
